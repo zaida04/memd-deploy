3c6f08b97942c0d6aeaf87744cb94735cdcabd5e
zaida04 <nico.03727+github@gmail.com>
2023-11-01 00:38:10 -0400

    feat: bot

---
+++ bot/src/env.ts
@@ -0,0 +1,2 @@
+export const DISCORD_TOKEN = process.env.DISCORD_TOKEN;
+export const DEPLOY_URL = process.env.DEPLOY_URL;
---
+++ bot/src/index.ts
@@ -0,0 +1,45 @@
+import "dotenv/config";
+import { Client } from "discord.js";
+import { DEPLOY_URL } from "./env";
+const client = new Client({ intents: ["Guilds", "GuildMessages", "MessageContent"] });
+const WHITELISTED_IDS = ["500765481788112916", "328723945266348033"];
+
+client.on("ready", () => {
+    console.log(`Logged in as ${client.user?.tag}!`);
+});
+
+client.on("messageCreate", async (message) => {
+    if (message.content !== "!deploy") return;
+    if (!WHITELISTED_IDS.includes(message.author.id)) {
+        message.reply("You are not allowed to use this command.");
+        return
+    }
+
+    const request = await fetch(DEPLOY_URL, {
+        method: "POST"
+    });
+
+    if (request.ok) {
+        message.reply("Deploying!");
+    } else {
+        message.reply("Something went wrong.");
+    }
+
+    setTimeout(async () => {
+        const frontendTest = await fetch("https://tracing.cse.buffalo.edu/");
+        if (frontendTest.ok) {
+            message.reply("Frontend is up!");
+        } else {
+            message.reply("Frontend is down!");
+        }
+
+        const backendTest = await fetch("https://tracing.cse.buffalo.edu/api");
+        if (backendTest.ok) {
+            message.reply("Backend is up!");
+        } else {
+            message.reply("Backend is down!");
+        }
+    });
+});
+
+client.login(process.env.DISCORD_TOKEN);
